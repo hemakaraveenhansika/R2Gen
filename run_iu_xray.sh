1f55476fe4bd3e3f61637fe3c900a537347a468e
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2021-12-22 18:07:10 +0530

    update annotaions

--- run_iu_xray.sh
+++ run_iu_xray.sh
@@ -1,6 +1,5 @@
 python main.py \
 --image_dir /kaggle/input/iu-xray-dataset/dataset/iu_xray/images/ \
-#--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 --ann_path /data/annotation.json \
 --dataset_name iu_xray \
 --max_seq_length 60 \
@@ -11,3 +10,6 @@ python main.py \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223
+
+
+#--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
