8e6b600243cb04fde0e19ab7fa8ee7078e69d263
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-02 11:26:19 +0530

    update resume_r2gen

--- train_decoder.sh
+++ train_decoder.sh
@@ -11,6 +11,7 @@ python main.py \
 --gamma 0.1 \
 --seed 9223 \
 --resume_contrastive_model /kaggle/input/r2gen-contrastive-model/R2Gen/results/contrastive_model_best.pth \
+--resume_r2gen /kaggle/input/r2gen/R2Gen/results/r2gen_model_best.pth \
 --mode train_decoder
 
 
