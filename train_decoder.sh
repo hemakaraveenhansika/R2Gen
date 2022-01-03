170776c85698e6023462d204b1e7bc86d9f44344
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-03 14:41:21 +0530

    update resume_contrastive_model

--- train_decoder.sh
+++ train_decoder.sh
@@ -5,12 +5,13 @@ python main.py \
 --max_seq_length 60 \
 --threshold 3 \
 --batch_size 16 \
---epochs 12 \
+--epochs 30 \
 --save_dir results \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
 --resume_contrastive_model /kaggle/input/r2gen-contrastive-model-resnet/R2Gen/results/contrastive_model_best.pth \
+--resume_r2gen /kaggle/input/r2gen-resnet/R2Gen/results/r2gen_model_best.pth \
 --mode train_decoder
 
 
