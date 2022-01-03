d035e62196e13c7249866400afb9ad4735488426
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-03 11:03:55 +0530

    update epochs

--- train_decoder.sh
+++ train_decoder.sh
@@ -1,11 +1,11 @@
 python main.py \
 --image_dir /kaggle/input/iu-xray-dataset/dataset/iu_xray/images/ \
---ann_path data/annotation.json \
+--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 --dataset_name iu_xray \
 --max_seq_length 60 \
 --threshold 3 \
 --batch_size 16 \
---epochs 4 \
+--epochs 1 \
 --save_dir results \
 --step_size 50 \
 --gamma 0.1 \
@@ -15,3 +15,4 @@ python main.py \
 
 
 #--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
+#--ann_path data/annotation.json \
