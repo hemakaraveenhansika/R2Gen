2339e0dc9f408d2c6a8f041d3593b5d1440fe3b7
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-10 22:43:43 +0530

    update resume_r2gen

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 python main.py \
 --image_dir /kaggle/input/iu-xray-dataset/dataset/iu_xray/images/ \
---ann_path data/annotation.json \
+--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 --dataset_name iu_xray \
 --max_seq_length 60 \
 --threshold 3 \
@@ -11,8 +11,9 @@ python main.py \
 --gamma 0.1 \
 --seed 9223 \
 --resume_contrastive_model /kaggle/input/r2gen-contrastive-model/R2Gen/results/contrastive_model_best.pth \
---resume_r2gen ./results/r2gen_model_best.pth \
+--resume_r2gen /kaggle/input/r2gen/R2Gen/results/r2gen_model_best.pth \
 --mode test
 
+#--resume_r2gen ./results/r2gen_model_best.pth \
 
 #--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
