2d2a0bc94c00b26114230a28848f43adaf09321a
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-10 19:56:16 +0530

    update ann_path and resum direct

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 python main.py \
 --image_dir /kaggle/input/iu-xray-dataset/dataset/iu_xray/images/ \
---ann_path data/annotation.json \
+--ann_path ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 --dataset_name iu_xray \
 --max_seq_length 60 \
 --threshold 3 \
@@ -10,9 +10,11 @@ python main.py \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
---resume_contrastive_model ./results/contrastive_model_best.pth \
---resume_r2gen ./results/r2gen_model_best.pth \
+--resume_contrastive_model /kaggle/input/r2gen-contrastive-model-resnet/R2Gen/results/contrastive_model_best.pth \
+--resume_r2gen /kaggle/input/r2gen-resnet/R2Gen/results/current_r2gen_checkpoint.pth \
 --mode test
 
+#--resume_contrastive_model ./results/contrastive_model_best.pth \
+#--resume_r2gen ./results/r2gen_model_best.pth \
 
 #--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
