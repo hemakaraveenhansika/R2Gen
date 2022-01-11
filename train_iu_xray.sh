216c4130cc6f628c699a168d660245cdb100159b
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-11 09:43:56 +0530

    update paths

--- train_iu_xray.sh
+++ train_iu_xray.sh
@@ -1,12 +1,12 @@
 python main.py \
 --image_dir /kaggle/input/iu-xray-dataset/dataset/iu_xray/images/ \
---ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
+--ann_path data/annotation.json \
 --dataset_name iu_xray \
 --max_seq_length 60 \
 --threshold 3 \
 --batch_size 16 \
 --epochs 2 \
---save_dir results/iu_xray \
+--save_dir results/ \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
