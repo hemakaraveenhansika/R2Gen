8be7324fe65d4a96e4651c3ecd089c2a1b5c99c8
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-11 14:59:57 +0530

    update epochs

--- train_iu_xray.sh
+++ train_iu_xray.sh
@@ -5,13 +5,16 @@ python main.py \
 --max_seq_length 60 \
 --threshold 3 \
 --batch_size 16 \
---epochs 15 \
+--epochs 30 \
 --save_dir results/ \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
---resume /kaggle/input/r2gen-base/R2Gen/results/r2gen_mode_base_best.pth \
+--resume /kaggle/input/r2genbasedata/R2Gen/results/r2gen_mode_base_best.pth \
 --mode train
 
 #--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 #--ann_path data/annotation.json \
+
+#--resume /kaggle/input/r2gen-base/R2Gen/results/r2gen_mode_base_best.pth \
+#--resume /kaggle/input/r2genbasedata/R2Gen/results/r2gen_mode_base_best.pth \
