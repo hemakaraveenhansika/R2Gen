b1abf062da2600104ea0687c021339a0523e2b7a
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-11 09:34:54 +0530

    update paths

--- test_iu_xray.sh
+++ test_iu_xray.sh
@@ -1,6 +1,6 @@
 python main.py \
 --image_dir /kaggle/input/iu-xray-dataset/dataset/iu_xray/images/ \
---ann_path data/annotation.json \
+--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 --dataset_name iu_xray \
 --max_seq_length 60 \
 --threshold 3 \
@@ -10,8 +10,10 @@ python main.py \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
---resume ./results/r2gen_mode_base_best.pth \
+--resume /kaggle/working/R2Gen/results/r2gen_mode_base_best.pth \
 --mode test
 
 #--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 #--ann_path data/annotation.json \
+
+
