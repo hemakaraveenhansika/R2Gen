7e5cf214e7f994db246c2938411c15eed5e816de
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-12 22:02:20 +0530

    update resume_r2gen

--- train_decoder.sh
+++ train_decoder.sh
@@ -5,13 +5,13 @@ python main.py \
 --max_seq_length 60 \
 --threshold 3 \
 --batch_size 16 \
---epochs 15 \
+--epochs 30 \
 --save_dir results \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
 --resume_contrastive_model /kaggle/input/r2gencontrastivemodel-v-1/R2Gen/results/contrastive_model_best.pth \
---resume_r2gen /kaggle/input/r2genbestmodel-v-1/R2Gen/results/r2gen_model_best.pth \
+--resume_r2gen /kaggle/input/r2genbestmodel-v-1/R2Gen/results/current_r2gen_checkpoint.pth \
 --mode train_decoder
 
 
