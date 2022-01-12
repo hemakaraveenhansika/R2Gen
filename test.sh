61d131c9f623f36c534fce5c3bc7e2d4cdf0beb2
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-12 17:58:15 +0530

    update resume_contrastive_model

--- test.sh
+++ test.sh
@@ -10,11 +10,16 @@ python main.py \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
---resume_contrastive_model /kaggle/input/r2gen-contrastive-model/R2Gen/results/contrastive_model_best.pth \
+--resume_contrastive_model /kaggle/input/r2gencontrastivemodel/R2Gen/results/contrastive_model_best.pth \
 --resume_r2gen /kaggle/input/r2gen-model-best/r2gen_model_best.pth \
 --mode test
 
-#--resume_r2gen ./results/r2gen_model_best.pth \
-#-resume_r2gen /kaggle/input/r2gen/R2Gen/results/r2gen_model_best.pth \
 
 #--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
+#--ann_path data/annotation.json \
+
+#--resume_contrastive_model /kaggle/input/r2gen-contrastive-model/R2Gen/results/contrastive_model_best.pth \
+#--resume_contrastive_model /kaggle/input/r2gencontrastivemodel/R2Gen/results/contrastive_model_best.pth \
+
+#--resume_r2gen ./results/r2gen_model_best.pth \
+#-resume_r2gen /kaggle/input/r2gen/R2Gen/results/r2gen_model_best.pth \
--- train_decoder.sh
+++ train_decoder.sh
@@ -10,10 +10,13 @@ python main.py \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
---resume_contrastive_model /kaggle/input/r2gen-contrastive-model/R2Gen/results/contrastive_model_best.pth \
+--resume_contrastive_model /kaggle/input/r2gencontrastivemodel/R2Gen/results/contrastive_model_best.pth \
 --resume_r2gen /kaggle/input/r2gen/R2Gen/results/r2gen_model_best.pth \
 --mode train_decoder
 
 
 #--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 #--ann_path data/annotation.json \
+
+#--resume_contrastive_model /kaggle/input/r2gen-contrastive-model/R2Gen/results/contrastive_model_best.pth \
+#--resume_contrastive_model /kaggle/input/r2gencontrastivemodel/R2Gen/results/contrastive_model_best.pth \
