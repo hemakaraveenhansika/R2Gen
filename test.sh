578644130e8c0724fcf8607d2b747e5dfa3ac5c8
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-12 20:27:33 +0530

    update resume_contrastive_model

--- test.sh
+++ test.sh
@@ -10,8 +10,8 @@ python main.py \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
---resume_contrastive_model /kaggle/input/r2gencontrastivemodel/R2Gen/results/contrastive_model_best.pth \
---resume_r2gen /kaggle/input/r2gen-model-best/r2gen_model_best.pth \
+--resume_contrastive_model /kaggle/input/r2gencontrastivemodel-v-1/R2Gen/results/contrastive_model_best.pth \
+--resume_r2gen /kaggle/working/R2Gen/results/r2gen_model_best.pth \
 --mode test
 
 
@@ -20,6 +20,7 @@ python main.py \
 
 #--resume_contrastive_model /kaggle/input/r2gen-contrastive-model/R2Gen/results/contrastive_model_best.pth \
 #--resume_contrastive_model /kaggle/input/r2gencontrastivemodel/R2Gen/results/contrastive_model_best.pth \
+#--resume_contrastive_model /kaggle/input/r2gencontrastivemodel-v-1/R2Gen/results/contrastive_model_best.pth \
 
-#--resume_r2gen ./results/r2gen_model_best.pth \
+#--resume_r2gen /kaggle/working/R2Gen/results/r2gen_model_best.pth \
 #-resume_r2gen /kaggle/input/r2gen/R2Gen/results/r2gen_model_best.pth \
--- train_decoder.sh
+++ train_decoder.sh
@@ -5,12 +5,12 @@ python main.py \
 --max_seq_length 60 \
 --threshold 3 \
 --batch_size 16 \
---epochs 30 \
+--epochs 1 \
 --save_dir results \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9223 \
---resume_contrastive_model /kaggle/input/r2gencontrastivemodel/R2Gen/results/contrastive_model_best.pth \
+--resume_contrastive_model /kaggle/input/r2gencontrastivemodel-v-1/R2Gen/results/contrastive_model_best.pth \
 --resume_r2gen /kaggle/input/r2gen/R2Gen/results/r2gen_model_best.pth \
 --mode train_decoder
 
@@ -20,3 +20,7 @@ python main.py \
 
 #--resume_contrastive_model /kaggle/input/r2gen-contrastive-model/R2Gen/results/contrastive_model_best.pth \
 #--resume_contrastive_model /kaggle/input/r2gencontrastivemodel/R2Gen/results/contrastive_model_best.pth \
+#--resume_contrastive_model /kaggle/input/r2gencontrastivemodel-v-1/R2Gen/results/contrastive_model_best.pth \
+
+#--resume_r2gen /kaggle/input/r2gen/R2Gen/results/r2gen_model_best.pth \
+#--resume_r2gen /kaggle/working/R2Gen/results/r2gen_model_best.pth \
