9f20cdc3161eecfe686c176b989d2795009a15bf
hemakaraveenhansika <hemakaraveenhansika@gmail.com>
2022-01-14 13:06:32 +0530

    update epochs

--- train_encoder.sh
+++ train_encoder.sh
@@ -5,14 +5,17 @@ python main.py \
 --max_seq_length 60 \
 --threshold 3 \
 --batch_size 16 \
---epochs 30 \
+--epochs 55 \
 --save_dir results \
 --step_size 50 \
 --gamma 0.1 \
 --seed 9225 \
---resume_contrastive_model /kaggle/input/r2gen/R2Gen/results/contrastive_model_best.pth \
+--resume_contrastive_model /kaggle/input/r2gencontrastivemodel-v-1/R2Gen/results/contrastive_model_best.pth \
 --mode train_encoder
 
 
 #--ann_path /kaggle/input/iu-xray-dataset/dataset/iu_xray/annotation.json \
 #--ann_path data/annotation.json \
+
+#--resume_contrastive_model /kaggle/input/r2gen/R2Gen/results/contrastive_model_best.pth \
+#--resume_contrastive_model /kaggle/input/r2gencontrastivemodel-v-1/R2Gen/results/contrastive_model_best.pth \
