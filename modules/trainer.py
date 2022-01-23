5999c71b9c256df9e6eafe4c2b90f53e3c890ad8
Vidura Prasangana <vidura.prasangana16@gmail.com>
2022-01-23 15:21:51 +0530

    fix gradient log

--- modules/trainer.py
+++ modules/trainer.py
@@ -276,7 +276,7 @@ class BaseR2GenTrainer(object):
         not_improved_count = 0
         complete_reslts = {}
         print("start r2gen model train - lr_ve -> ", self.args.lr_ve)
-        wandb.watch(self.visual_extractor_model,log='all')
+        wandb.watch(self.visual_extractor_model,log='all', log_freq=1)
         for epoch in range(self.start_epoch, self.epochs + 1):
             epoch_reslts = {}
             result = self._train_epoch(epoch)
